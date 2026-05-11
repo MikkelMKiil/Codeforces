59dfd3a802d293ca3d94ab75dc985afc083ce6d2
MikkelMKiil <mikkeltom@hotmail.dk>
2026-05-11 10:45:33 +0200

    617A solved, patterns done for today

--- PatternMemorization/10patterns.cpp
+++ PatternMemorization/10patterns.cpp
@@ -2,103 +2,49 @@
 using namespace std;
 
 // 1 read n and array
-int one(){
- int n, min, max;
- cin >> n;
- vector<int> a(n);
- for(auto& x : a) cin >> x;
+int main(){
+   int n;
+   cin >> n;
+   vector<int> a(n);
+   for (auto& x : a) cin >> x;
 }
-
 // 2 sort
-int two(){
- int n, min, max;
- cin >> n;
- vector<int> a(n);
- for(auto& x : a) cin >> x;
- sort(a.begin(),a.end());
-}
+sort(a.begin(), a.end());
+sort(a.begin(), a.end(), greater<>());
 // 3 walk adjacent pairs
-int three(){
- int n, min, max;
- cin >> n;
- vector<int> a(n);
- for(auto& x : a) cin >> x;
- sort(a.begin(),a.end());
- for(int i = 1; i < n; ++i){
-    if(a[i - 1] < a[i]){
-        cout << "larger";
-    }
- }
+for(int i = 1; i < n; ++i){
+   if(a[i-1] > a[i]){ // adjacent logic here
+      }
 }
 // 4 boolean yes no
-int four(){
- int n, min, max;
- bool larger = true;
- cin >> n;
- vector<int> a(n);
- for(auto& x : a) cin >> x;
- sort(a.begin(),a.end());
- for(int i = 1; i < n; ++i){
-    if(a[i-1] < a[i]){
-        larger = true;
-        break;
-    }
-}
- cout << (larger ? "YES" : "NO");
-}
+seen = true;
+cout << (seen ? "YES" : "NO") << "\n";
+
+
 // 5 multi test case
-int five(){
- int t, n;
- cin >> t >> n;
- while(t--){
-    // do something
- }
-    
+int t;
+cin >> t;
+while(t--){
+   // runs every case
 }
 // 6 count with map
-int six(){
- int n, mini, maxi;
- cin >> n;
- vector<int> a;
- map<int,int> cnt;
- for(int x : a) cnt[x]++;
- for(auto& [k, v] : cnt){
+map<int,bool> unique;
 
- }
-}
+
 // 7 find min max
-int seven(){
- int n, lo, hi;
- cin >> n;
- vector<int> a(n);
- for(auto& x : a) cin >> x;
- lo = *min_element(a.begin(), a.end());
- hi = *max_element(a.begin(), a.end());
- cout << lo << hi << "\n";
-}
+int max = *max_element(a.begin(),a.end());
+int min = *min_element(a.begin(),a.end());
 // 8 sum
-int eight(){
- int n;
- cin >> n;
- vector<int> a(n);
- for(auto& x : a) cin >> x;
- cout << accumulate(a.begin(), a.end(), 0LL);
-}
+long long sum = accumulate(a.begin(),a.end(), 0LL);
+
 // 9 two pointers from ends
-int nine(){
- int n;
- cin >> n;
- int l = 0, r = n - 1;
- while(l < r){
-    // if x > y, r--
-    // if x < y, l++
- }
+int l = 0, r = n-1;
+while(l < r){
+   if(logic goes here) r++, l++;
+   else if (more logic) r++;
+   else(force move)
+   l++;
 }
 // 10 output array with spaces
-int ten(){
- int n;
- cin >> n;
- vector<int> a(n);
- for(auto& x : a) cin >> x;
- for(auto& x : a) cout << x << " ";
-}
+
+cout << x << " ";
